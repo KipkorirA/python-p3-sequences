10797861df2c24436e694873f54c0147c06b63e2
KipkorirA <aron.kipkorir@student.moringaschool.com>
2024-08-30 19:08:40 +0300

    Done

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        print([])
+        return 
+    elif length == 1:
+        print([0])
+        return
+    elif length == 2:
+        print([0, 1])
+        return
+    
+    a, b = 0, 1 
+    result = [a, b]
+    
+    while len(result) < length:
+        a, b = b, a + b
+        result.append(b)
+        
+    print(result)
